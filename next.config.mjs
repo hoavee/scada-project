5aab6f7cfa3d9a6a2876a6d82bfc027ea201a0af
hoavee <hoavee@gmail.com>
2026-01-16 15:41:18 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        // Khi gọi /api-proxy/..., Next.js sẽ tự hiểu là gọi đến IP 113.164.80.153
+        source: "/api-proxy/:path*",
+        destination: "http://113.164.80.153:8000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
